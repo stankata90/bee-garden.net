8173e2689f9d63378f0afc8ba0f4d9ba14425b2a
stankata90 <slavi_we@abv.bg>
2018-12-12 18:16:03 +0200

    update

--- debug.php
+++ debug.php
@@ -1,15 +1,22 @@
 <?php
     $intA = readline();
     $intB = readline();
+
     $result = 0;
     $strNember = '';
+
     while( $intA != 0 ) {
+
         if( $intA % 2 == $intB ) {
             $result++;
         }
+
+
         $strNember .= $intA % 2;
         $intA =  floor( $intA / 2 );
     }
+
     $strNember = strrev ( $strNember );
+
     echo $strNember . PHP_EOL;
     echo $result;
